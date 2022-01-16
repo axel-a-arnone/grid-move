0c1f0c3fc0b1d98b87956775c4821b06961e8134
Axel A. Arnone <axelaleksandr.arnone@studio.unibo.it>
2022-01-16 23:04:45 +0100

    Added valid position checker to Car object

--- track_environment.py
+++ track_environment.py
@@ -8,8 +8,24 @@ Created on Sun Jan 16 15:18:28 2022
 
 class Track:
     def __init__(self, width, height):
+        """
+        A class to represent a Track
+
+        Parameters
+        ----------
+        width : INT
+            Number of grid-cells in x direction.
+        height : INT
+            Number of grid-cells in y direction.
+
+        Returns
+        -------
+        None.
+
+        """
         self.height = self._is_valid_height(height)
         self.width = self._is_valid_width(width)
+        self.track_cells = self._track_base()
 
     def _is_valid_height(self, height):
         if not isinstance(height, int):
@@ -23,6 +39,20 @@ class Track:
         else:
             return width
 
+    def _track_base(self):
+        """
+        Fills the cells with 'r', for road cells
+
+        Returns
+        -------
+        None.
+
+        """
+        track_dict = {}
+        for x_idx in range(self.width):
+            for y_idx in range(self.height):
+                track_dict[x_idx, y_idx] = 'r'
+
 
 class Car:
     def __init__(self, track, x_pos=0, y_pos=0, x_speed=0, y_speed=0):
@@ -35,5 +65,40 @@ class Car:
         self.speed[1] = self.speed[1]+y_change
 
     def move_once(self):
-        self.position[0] += self.speed[0]
-        self.position[1] += self.speed[1]
+        """
+        Updates car position according to current speed'
+
+        Returns
+        -------
+        None.
+
+        """
+        new_x = self.position[0] + self.speed[0]
+        new_y = self.position[1] + self.speed[1]
+        new_position = self._is_valid_position(self, new_x, new_y)
+        self.position[0] = new_position[0]
+        self.position[1] = new_position[1]
+
+    def _is_valid_position(self, x_pos, y_pos):
+        """
+        Checks if the position is legal
+
+        Parameters
+        ----------
+        x_pos : INT
+            X coordinate of position.
+        y_pos : INT
+            Y coordinate of position.
+
+        Returns
+        -------
+        If position is legal, returns [x_pos, y_pos].
+
+        """
+        track_cells = self.track.track_cells
+        try:
+            cell_type = track_cells[x_pos, y_pos]
+        except KeyError:
+            print("Position out of bounds, can't move")
+        if cell_type == 'r':
+            return[x_pos, y_pos]
