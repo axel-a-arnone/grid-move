6f9f3951740c982aa48e83fa30278e9efc17981d
Axel A. Arnone <axelaleksandr.arnone@studio.unibo.it>
2022-01-17 00:00:41 +0100

    Added print functions to see track, car and speed

--- track_environment.py
+++ track_environment.py
@@ -4,6 +4,7 @@ Created on Sun Jan 16 15:18:28 2022
 
 @author: Axel
 """
+import matplotlib.pyplot as plt
 
 
 class Track:
@@ -25,7 +26,7 @@ class Track:
         """
         self.height = self._is_valid_height(height)
         self.width = self._is_valid_width(width)
-        self.track_cells = self._track_base()
+        self.track_grid = self._track_base()
 
     def _is_valid_height(self, height):
         if not isinstance(height, int):
@@ -41,17 +42,39 @@ class Track:
 
     def _track_base(self):
         """
-        Fills the cells with 'r', for road cells
+        Fills the cells with 'r', for cells of type road
 
         Returns
         -------
         None.
 
         """
-        track_dict = {}
+        track_grid = {}
         for x_idx in range(self.width):
             for y_idx in range(self.height):
-                track_dict[x_idx, y_idx] = 'r'
+                track_grid[x_idx, y_idx] = 'r'
+        return track_grid
+
+    def print_track(self):
+        """
+        Shows current track as a grid of dots.
+        Black dots represent cells of type road
+
+        Returns
+        -------
+        None.
+
+        """
+        # Obtaining x-y pairs from track_grid dict
+        x_road_vector = []
+        y_road_vector = []
+        for position in self.track_grid.keys():
+            cell_type = self.track_grid[position]
+            if cell_type == 'r':
+                x_road_vector.append(position[0])
+                y_road_vector.append(position[1])
+        # Plotting track as scatter plot
+        plt.plot(x_road_vector, y_road_vector, 'ko')
 
 
 class Car:
@@ -65,17 +88,9 @@ class Car:
         self.speed[1] = self.speed[1]+y_change
 
     def move_once(self):
-        """
-        Updates car position according to current speed'
-
-        Returns
-        -------
-        None.
-
-        """
         new_x = self.position[0] + self.speed[0]
         new_y = self.position[1] + self.speed[1]
-        new_position = self._is_valid_position(self, new_x, new_y)
+        new_position = self._is_valid_position(new_x, new_y)
         self.position[0] = new_position[0]
         self.position[1] = new_position[1]
 
@@ -95,10 +110,29 @@ class Car:
         If position is legal, returns [x_pos, y_pos].
 
         """
-        track_cells = self.track.track_cells
+        track_grid = self.track.track_grid
+        current_position = self.position
         try:
-            cell_type = track_cells[x_pos, y_pos]
+            cell_type = track_grid[x_pos, y_pos]
         except KeyError:
             print("Position out of bounds, can't move")
+            return current_position
         if cell_type == 'r':
             return[x_pos, y_pos]
+
+    def print_current_status(self):
+        """
+        Prints track, car and current speed
+
+        Returns
+        -------
+        None.
+
+        """
+        self.track.print_track()
+        plt.plot(self.position[0], self.position[1], 'bo')
+        plt.arrow(self.position[0], self.position[1],
+                  self.speed[0], self.speed[1],
+                  width=0.05,
+                  length_includes_head=True,
+                  color='g')
